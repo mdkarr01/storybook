ba419ff76042cf5896153536b3dd41abb22afec6
Michael Karr <mdkarr01@gmail.com>
2018-02-07 17:32:55 -0600

    update app

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(passport.session());
 
 //FLASH MESSAGING
 app.use(flash());
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.locals.user = req.user;
   res.locals.error = req.flash("error");
   res.locals.success_msg = req.flash("success_msg");
@@ -85,10 +85,10 @@ app.use("/stories", storiesRoutes);
 
 //=====================================================================
 
-// app.listen(process.env.PORT, process.env.IP || 5000, () => {
-//   console.log("The StoryBook Server Has Started Port 5000!");
-// });
-
-app.listen(5000 || process.env.PORT, process.env.IP, () => {
+app.listen(process.env.PORT, process.env.IP || 5000, () => {
   console.log("The StoryBook Server Has Started Port 5000!");
 });
+
+// app.listen(5000 || process.env.PORT, process.env.IP, () => {
+//   console.log("The StoryBook Server Has Started Port 5000!");
+// });
