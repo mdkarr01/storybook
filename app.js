72d9a7d2ea4d8388cba68ca9ce95e179f3cd4b87
Michael Karr <mdkarr01@gmail.com>
2018-02-26 17:43:11 -0600

    app

--- app.js
+++ app.js
@@ -1,110 +1,94 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const exphbs = require("express-handlebars");
-const methodOverride = require("method-override");
-const flash = require("connect-flash");
-const bodyParser = require("body-parser");
-const expressSanitizer = require("express-sanitizer");
-const cookieParser = require("cookie-parser");
-const passport = require("passport");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
-const app = express();
+// Load Models
+require('./models/User');
+require('./models/Story');
 
-//Load User
-require("./models/User");
-require("./models/Story");
+// Passport Config
+require('./config/passport')(passport);
 
-//Load Passport Module
-require("./config/passport")(passport);
+// Load Routes
+const index = require('./routes/index');
+const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
-//Load auth Routes
-const indexRoutes = require("./routes/index");
-const authRoutes = require("./routes/auth");
-const storiesRoutes = require("./routes/stories");
+// Load Keys
+const keys = require('./config/keys');
 
-// MLAB CONFIG
-var uri = process.env.DBLOGIN;
-
-mongoose
-  .connect(uri)
-  .then(() => console.log("Db Connected"))
-  .catch(err => console.log(err));
-
-mongoose.Promise = global.Promise;
-
-//HANDLEBARS HELPERS
+// Handlebars Helpers
 const {
   truncate,
   stripTags,
   formatDate,
-  formatDashboardDate,
   select,
   editIcon
-} = require("./helpers/hbs");
+} = require('./helpers/hbs');
 
-//HANDLEBARS MIDDLEWARE
-app.engine(
-  "handlebars",
-  exphbs({
-    helpers: {
-      truncate: truncate,
-      stripTags: stripTags,
-      formatDate: formatDate,
-      formatDashboardDate: formatDashboardDate,
-      select: select,
-      editIcon: editIcon
-    },
-    defaultLayout: "main",
-    secondaryLayout: "home"
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+    useMongoClient: true
   })
-);
-app.set("view engine", "handlebars");
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-//STATIC PARSER
-var dir = path.join(__dirname, "/public");
-app.use(express.static(dir));
+const app = express();
 
-app.use(methodOverride("_method"));
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
 
-//BODY PARSER MIDDLEWARE
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(bodyParser.json());
+// MEthod Override Middelware
+app.use(methodOverride('_method'));
 
-app.use(expressSanitizer()); // this line follows bodyParser() instantiations
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  helpers: {
+    truncate: truncate,
+    stripTags: stripTags,
+    formatDate: formatDate,
+    select: select,
+    editIcon: editIcon
+  },
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
 
-app.use(
-  require("express-session")({
-    secret: "Nellie is a baby girl",
-    resave: false,
-    saveUninitialized: false
-  })
-);
-
-//Passport Middleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-//FLASH MESSAGING
-app.use(flash());
-app.use(function (req, res, next) {
-  res.locals.user = req.user;
-  res.locals.error = req.flash("error");
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.error_msg = req.flash("error_msg");
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
   next();
 });
 
-app.use("/", indexRoutes);
-app.use("/auth", authRoutes);
-app.use("/stories", storiesRoutes);
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use Routes
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/stories', stories);
 
 //=====================================================================
 
