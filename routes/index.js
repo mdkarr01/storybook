e4abeb9502c884a4103cc5236c8c90fd83aa0d42
Michael Karr <mdkarr01@gmail.com>
2018-02-26 14:27:23 -0600

    changed the route back to welcfomer

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
-const Story = mongoose.model("stories");
-const User = mongoose.model("users");
+const mongoose = require('mongoose');
+const Story = mongoose.model('stories');
+const {
+  ensureAuthenticated,
+  ensureGuest
+} = require('../helpers/auth');
 
-router.get("/", (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
+  res.render('index/welcome');
+});
+
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
   Story.find({
-    status: "public"
-  })
-    .sort({
-      date: "desc"
+      user: req.user.id
     })
-    .populate("user")
     .then(stories => {
-      res.render("stories/index", {
+      res.render('index/dashboard', {
         stories: stories
       });
     });
 });
 
-router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  Story.find({
-    user: req.user.id
-  }).then(stories => {
-    res.render("index/dashboard", {
-      stories: stories
-    });
-  });
-});
-
-router.get("/about", (req, res) => {
-  res.render("index/about");
+router.get('/about', (req, res) => {
+  res.render('index/about');
 });
 
 module.exports = router;
